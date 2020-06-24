f8cda2e78f0f026061b0297fadb3592f5cc2e373
evyatar <evyatar1919@gmail.com>
2020-06-24 15:44:50 +0300

    registration validation on type

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,8 @@ const routes = express.Router();
 
 routes.get('/users', users.getAll);
 routes.put('/users', users.create);
+routes.get('/users/check', users.check);
+routes.post('/users/login', users.login);
 
 routes.get('/health', (req, res) => {
 	res.send();
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,6 @@
+const md5 = require('md5');
 const User = require('../models/user');
+const ERROR_DUPLICATE_VALUE = 11000;
 
 class Users {
 
@@ -8,13 +10,54 @@ class Users {
 			.catch(err => res.status(500).json(err));
 	}
 
+	async check(req, res) {
+		const { username, email } = req.query;
+
+		if (!username && !email) {
+			res.sendStatus(400);
+			return;
+		}
+		let property = email ? 'email' : 'username';
+
+		try {
+			const isExist = await User.exists({
+				[property]: req.query[property]
+			});
+			res.json(isExist);
+		} catch(err) {
+			res.status(400).json(err);
+		}
+	}
+
 	async create(req, res) {
 		const newUser = new User(req.body);
+		newUser.password = md5(newUser.password);
 		try {
 			const createdUser = await newUser.save();
 			res.status(201).json(createdUser);
 		} catch(err) {
+			if (err.code === ERROR_DUPLICATE_VALUE) {
+				res.sendStatus(409);
+				return;
+			}
 			res.status(400).json(err);
+		}
+	}
+
+	async login(req, res) {
+		const credentials = req.body;
+		try {
+			const user = await User.findOne({
+				username: credentials.username,
+				password: md5(credentials.password)
+			});
+			if (!user) {
+				res.sendStatus(401);
+				return;
+			}
+			res.json(user).send();
+		} catch(err) {
+			res.sendStatus(500);
 		}
 	}
 
